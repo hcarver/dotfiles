e086ca66f4c7226be49315a7e1d439d9b6bd47a7
Hywel Carver <>
2022-03-04 17:11:59 +0000

    Update fish vars

--- fish/fish.symlink/conf.d/omf.fish
+++ fish/fish.symlink/conf.d/omf.fish
@@ -3,5 +3,8 @@ set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG '/Users/h/.dotfiles/omf/config'
+
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
